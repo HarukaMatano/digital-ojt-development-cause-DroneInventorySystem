a2311cb04f9efef089a0757240e09dd979f0dc57
unknown <z2240235@intra.ctc-g.co.jp>
2024-12-10 17:15:17 +0900

    稼働ステータス部分完了

--- DroneInventorySystem/src/main/java/com/digitalojt/web/repository/CenterInfoRepository.java
+++ DroneInventorySystem/src/main/java/com/digitalojt/web/repository/CenterInfoRepository.java
@@ -31,7 +31,6 @@ public interface CenterInfoRepository extends JpaRepository<CenterInfo, Integer>
 			"(:region = '' OR s.address LIKE %:region%) AND " +
 			"(:storageCapacityFrom IS NULL OR s.currentStorageCapacity >= :storageCapacityFrom) AND " +
 			"(:storageCapacityTo IS NULL OR s.currentStorageCapacity <= :storageCapacityTo) AND " +
-			"(s.operationalStatus = 0) AND" +
 			"(s.deleteFlag='0')"
 			)
 	List<CenterInfo> findByCenterNameAndRegionAndStorageCapacity
@@ -43,8 +42,8 @@ public interface CenterInfoRepository extends JpaRepository<CenterInfo, Integer>
 	);
 
 	@Query("SELECT s FROM CenterInfo s WHERE " +
-			"(s.deleteFlag='0') AND " +
-			"(s.operationalStatus = 0)")
+			"(s.deleteFlag='0')" 
+			)
 	List<CenterInfo> findByOperrationalStatusAndDeleteFlag();
 	
 	CenterInfo findByCenterId(Integer centerId);
